276ab45d8513771b65c925468e644e0ca7e50642
thanatadcs <thanatad.cs@gmail.com>
2023-03-24 17:16:03 +0700

    Changed some content in app.py again

--- src/simple_app/app.py
+++ src/simple_app/app.py
@@ -23,7 +23,7 @@ migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
-    return "hello world555 Updated"
+    return "hello world555 Updated haha"
 
 
 app.add_url_rule("/todo", methods=["GET"], view_func=todo_api.list_todo)
